518c79f3a3d50b94c5f5c59d61e64a473d1b19d8
Raul Barroso <89712072+BarrosoR@users.noreply.github.com>
2023-08-29 09:31:20 -0300

    Create Estruturas_De_Controle.rb

---
+++ AULAS/Estruturas_De_Controle.rb
@@ -0,0 +1,11 @@
+#analise um dia da semana
+#Se esse dia da semana for domingo
+#Imprima que o nosso almoço será especial
+
+dia = 'domingo'
+if dia == 'segunda' # == é uma comparação
+    almoco == 'especial'
+end
+
+#imprime --> puts
+puts "Hoje nosso almoço será #{almoco}"
