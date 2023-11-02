dfbcdfe5c5afc5571c913ac94cf86c4f44ce9a0e
Raul Barroso <89712072+BarrosoR@users.noreply.github.com>
2023-11-02 20:46:56 -0300

    Organizacao de pasta
    Organizacao de pasta Ruby

---
+++ RUBY/AULAS/DesafiosT.rb
@@ -0,0 +1,8 @@
+number = gets.to_i
+num = 25
+
+#TODO: Crie as condições necessárias para verificar se a variável
+//é maior que a constante e print no console a frase
+
+num < 25 # a menos que x seja menor que 25
+puts "Está calor!"
